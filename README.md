1656d18fd0a06a5d7dd1a44bd37dda7503cb6cab
Soham Pardeshi <sohampardeshi@gmail.com>
2017-08-19 17:50:00 -0700

    Restarting

--- README.md
+++
@@ -1,5 +0,0 @@
-# Soham Pardeshi - Personal Website
-
-Created on June 21, 2017. NameCheap Hosting.
-
-### Todo
--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
-window.onload = function() {
-
-	console.log('Hello World');
-	
-
-
-
-}
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC1nbQ_QMFHAposHeZVJyfNjBT8F53cPss",
+    authDomain: "markdownnotes-b88c1.firebaseapp.com",
+    databaseURL: "https://markdownnotes-b88c1.firebaseio.com",
+    projectId: "markdownnotes-b88c1",
+    storageBucket: "markdownnotes-b88c1.appspot.com",
+    messagingSenderId: "491819736828"
+};
+firebase.initializeApp(config);
