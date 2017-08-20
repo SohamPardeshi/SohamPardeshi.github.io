cbdeec7ffea72b3ce7da8d6fe9ab681f458d9113
Soham Pardeshi <sohampardeshi@gmail.com>
2017-08-19 20:00:49 -0700

    Added notes view

--- scripts.js
+++ scripts.js
@@ -10,10 +10,52 @@ var config = {
 firebase.initializeApp(config);
 
 
-var database = firebase.database();
-var ref = database.ref();
-alert(ref);
+var db = firebase.database();
+var ref = db.ref("files");
+var notes = []
 
-ref.on('value', function(snapshot){
-    alert(snapshot);
+
+ref.once('value').then(function(snap){
+    for(var key in snap.val()){
+        notes.push(snap.val()[key])
+    }
+    sortNotes();
+    display();
 });
+
+
+function sortNotes(){
+    function compare(a, b) {
+      if (a.dateModified > b.dateModified)
+        return -1;
+      if (a.dateModified < b.dateModified)
+        return 1;
+      return 0;
+    }
+    notes.sort(compare);
+}
+
+
+function display() {
+    if(notes.length == 0) alert("No notes found...");
+    
+    var container = document.getElementById('container');
+    
+    for(note in notes){
+        var obj = notes[note];
+        var date = new Date(obj.dateModified);
+        
+        var txt = 
+        "<a href='view.html?content=" + btoa(obj.contents) + "'><div class='note'>" + 
+            
+        "<div class='class'>" + obj.className +  "</div>" +
+        "<div class='center'>" + 
+            "<div class='name'>" + obj.filename +  "</div>" +
+            "<div class='date'>Last Modified: <span>" + date.toRelativeTime() +  "</span></div>" +
+        "</div>" +
+        "<div class='right'><div class='arrow'></div></div>"+
+        "</div></a>";
+        
+        container.innerHTML += txt;
+    }
+}
