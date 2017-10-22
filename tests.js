81ee63e083df0f4833de876d6cabd0c0d272c743
Soham Pardeshi <sohampardeshi@gmail.com>
2017-10-21 22:18:45 -0700

    test

---
+++ tests.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+    var url = "https://www.w3schools.com/w3images/fjords.jpg";
+    getImageLightness(url, function(b) {
+        console.log(b);
+    });
+}
+
+function getImageLightness(imageSrc, callback) {
+    var img = document.createElement("img");
+    img.src = imageSrc;
+    img.style.display = "none";
+    document.body.appendChild(img);
+
+    var colorSum = 0;
+
+    img.onload = function() {
+        var canvas = document.createElement("canvas");
+        canvas.width = this.width;
+        canvas.height = this.height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(this, 0, 0);
+
+        var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        var data = imageData.data;
+        var r, g, b, avg;
+
+        for(var x = 0, len = data.length; x < len; x+=4) {
+            r = data[x];
+            g = data[x + 1];
+            b = data[x + 2];
+
+            avg = Math.floor((r+g+b)/3);
+            colorSum += avg;
+        }
+
+        var brightness = Math.floor(colorSum / (this.width*this.height));
+        callback(brightness);
+    }
+}
